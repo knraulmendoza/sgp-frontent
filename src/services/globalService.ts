fd9dba3920a232534981ce209ac6fd7b9396c5c8
menkemon <kenne_8@outlook.es>
2019-11-06 16:50:59 -0500

    global services url

---
+++ src/services/globalService.ts
@@ -0,0 +1,5 @@
+class GloablService {
+    url = 'http://6e117652.ngrok.io/api';
+}
+
+export const globalServices = new GloablService();
--- src/services/sgpServices.ts
+++ src/services/sgpServices.ts
@@ -1,21 +1,21 @@
 import axios, { AxiosResponse } from 'axios';
 import { Isgp } from '@/interfaces/interface';
+import { globalServices } from './globalService';
 
 class SgpService {
 
-  public url = 'http://6e117652.ngrok.io/api/ingresoonceava';
     public sgps: Isgp[] = [
     ];
   // metodos del CRUD
   public async consultar() {
-      const data = await axios.get(this.url).then((sgps) => {
+      const data = await axios.get(globalServices.url+'ingresoonceava').then((sgps) => {
           return sgps.data;
         }).catch((_) => {});
       return data;
   }
 
   public add(sgp: Isgp) {
-      axios.post(this.url, sgp).then((_) => {console.log('ok'); }).catch((_) => {console.error('error'); });
+      axios.post(globalServices.url+'ingresoonceava', sgp).then((_) => {console.log('ok'); }).catch((_) => {console.error('error'); });
   }
 
   public update(sgp: Isgp) {
